8b2f1939dded1d2e5c5fbcec285c9836404bc41c
bbortt <timon.borter@post.ch>
2018-08-16 18:34:36 +0200

    use deployment scripts.

---
+++ scripts/deploy-snapshot.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Deployment script for TBME TV Constants
+# https://github.com/tbmelabs/tbmelabs-tv-constants
+
+if [[ $TRAVIS_BRANCH == "development" && $TRAVIS_PULL_REQUEST == "false" ]]; then
+  mvn deploy -Pbuild-extras -DskipTests
+fi
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,9 +3,5 @@
 # https://github.com/tbmelabs/tbmelabs-tv-constants
 
 if [[ $TRAVIS_BRANCH == "master" && $TRAVIS_PULL_REQUEST == "false" ]]; then
-  mvn clean versions:set deploy -Psign,build-extras -DremoveSnapshot
-elif [[ $TRAVIS_BRANCH == "development" && $TRAVIS_PULL_REQUEST == "false" ]]; then
-  mvn clean deploy -Pbuild-extras -DisSnapshot
-else
-  mvn clean verify
+  mvn clean versions:set deploy -Psign,build-extras -DremoveSnapshot -DskipTests
 fi
