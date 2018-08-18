131f2c773578750ec9bfd951dddec6d3ed2f3670
bbortt <timon.borter@post.ch>
2018-08-18 22:12:54 +0200

    reformat scripts.

--- scripts/deploy-snapshot.sh
+++ scripts/deploy-snapshot.sh
@@ -2,6 +2,7 @@
 # Deployment script for TBME TV Constants
 # https://github.com/tbmelabs/tbmelabs-tv-constants
 
-if [[ $TRAVIS_BRANCH == "development" && $TRAVIS_PULL_REQUEST == "false" ]]; then
+if [[ $TRAVIS_BRANCH == "development" && $TRAVIS_PULL_REQUEST == "false" ]] ; then
   mvn deploy -Pbuild-extras -DskipTests
 fi
+
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,7 @@
 # Deployment script for TBME TV Constants
 # https://github.com/tbmelabs/tbmelabs-tv-constants
 
-if [[ $TRAVIS_BRANCH == "master" && $TRAVIS_PULL_REQUEST == "false" ]]; then
+if [[ $TRAVIS_BRANCH == "master" && $TRAVIS_PULL_REQUEST == "false" ]] ; then
   mvn clean versions:set deploy -Psign,build-extras -DremoveSnapshot -DskipTests
 fi
+
