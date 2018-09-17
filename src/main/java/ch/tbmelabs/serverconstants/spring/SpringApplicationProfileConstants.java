92e4e0c6f67f57204b0619b78f31a153402716b7
bbortt <timon.borter@post.ch>
2018-09-17 20:58:09 +0200

    profile constants.

---
+++ src/main/java/ch/tbmelabs/serverconstants/spring/SpringApplicationProfileConstants.java
@@ -0,0 +1,10 @@
+package ch.tbmelabs.serverconstants.spring;
+
+public class SpringApplicationProfileConstants {
+
+  public static final String PROD = "prod";
+  public static final String DEV = "dev";
+  public static final String TEST = "test";
+  public static final String NO_REDIS = "no-redis";
+  public static final String NO_MAIL = "no-mail";
+}
